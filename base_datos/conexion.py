f90731af40921c50fcca887bba83d20c5199956a
DiegoMdz19 <diegomdz19@hotmail.com>
2025-10-16 20:18:53 +0200

    base de datos local

--- base_datos/conexion.py
+++ base_datos/conexion.py
@@ -4,7 +4,7 @@ import envyte
 db_url = envyte.get("DB_URL")
 api_token = envyte.get("API_TOKEN")
 
-conn = libsql.connect("proyectoaad", sync_url = db_url, auth_token = api_token)
+conn = libsql.connect("proyectoaad") #, sync_url = db_url, auth_token = api_token)
 cursor = conn.cursor()
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS clientes (
