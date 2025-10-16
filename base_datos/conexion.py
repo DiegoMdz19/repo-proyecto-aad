47d27b1f1ef96be659883c0a5ac554368fbc9509
DiegoMdz19 <diegomdz19@hotmail.com>
2025-10-16 20:15:50 +0200

    Conexion a DB y tabla clientes creada

--- base_datos/conexion.py
+++ base_datos/conexion.py
@@ -1,3 +1,19 @@
 import libsql
+import envyte
 
-conn = libsql.connect("")
+db_url = envyte.get("DB_URL")
+api_token = envyte.get("API_TOKEN")
+
+conn = libsql.connect("proyectoaad", sync_url = db_url, auth_token = api_token)
+cursor = conn.cursor()
+cursor.execute("""
+                CREATE TABLE IF NOT EXISTS clientes (
+                id INTEGER PRIMARY KEY,
+                nombre TEXT NOT NULL,
+                direccion TEXT NOT NULL,
+                CIF INTEGER NOT NULL
+               )
+               """)
+conn.commit()
+conn.sync()
+conn.close()
